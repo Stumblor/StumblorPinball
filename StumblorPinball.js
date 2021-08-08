658e026a6c755c7b290b21356a7d58db4dfb78af
Stumblor <davey@codercabana.co.uk>
2021-08-08 18:34:26 +0100

    Create StumblorPinball.js

---
+++ StumblorPinball.js
@@ -0,0 +1,52 @@
+JSResult Skip Results Iframe
+EDIT ON
+// Global site tag (gtag.js) - Google Analytics
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-FM7JMR8V4S');
+gtag('config', 'UA-186317069-1');
+
+//---------------------------------------
+// DRIFT
+!function() {
+  var t = window.driftt = window.drift = window.driftt || [];
+  if (!t.init) {
+    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+    t.factory = function(e) {
+      return function() {
+        var n = Array.prototype.slice.call(arguments);
+        return n.unshift(e), t.push(n), t;
+      };
+    }, t.methods.forEach(function(e) {
+      t[e] = t.factory(e);
+    }), t.load = function(t) {
+      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+      var i = document.getElementsByTagName("script")[0];
+      i.parentNode.insertBefore(o, i);
+    };
+  }
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('4333vua8587v');  
+
+// CUSTOM CSS
+var styleElement = document.createElement('style');
+
+var css = [
+  ".zoomable__image[format=gif] { object-fit: contain!important; }", // Animated GIFs on Product Page
+  "#banner-text { font-size: 24px!important; }" // homepage banner fixes
+];
+for(var i = 0; i < css.length; i++)
+  styleElement.appendChild(document.createTextNode(css[i]));
+  
+document.getElementsByTagName('head')[0].appendChild(styleElement);
+
+// TOP BANNER EVERY LOAD
+setInterval(function() {
+  if (window.leadForm && window.leadForm["form_ids"].length > 1) document.cookie = "leadform_" + window.leadForm["form_ids"][1] + "_viewed=; Max-Age=-99999999;";
+}, 1000);
