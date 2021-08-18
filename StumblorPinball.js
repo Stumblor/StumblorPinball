b849d7f57f24a54f4e75318fe84b15cee7d7c5e0
Stumblor <davey@codercabana.co.uk>
2021-08-18 10:23:10 +0100

    Update StumblorPinball.js

--- StumblorPinball.js
+++ StumblorPinball.js
@@ -60,9 +60,10 @@ setInterval(function() {
 
 // Stories
 if ($.ajax) {
-  $.ajax({ url: "https://www.stumblorpinball.com/s/stories" });
+  $.ajax({ url: "https://www.stumblorpinball.com/s/stories", dataType: "html" });
   var stories = () => {
     if ($('#dniXkG').length == 0) setTimeout(stories, 500);
+    debugger;
     $('#dniXkG').after("<div>Testing</div>");
   }
   stories();
