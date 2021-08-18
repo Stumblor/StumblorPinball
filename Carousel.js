bf9b4ee13755c0d924d36e295a4dbbd50a6a5996
Stumblor <davey@codercabana.co.uk>
2021-08-18 10:10:58 +0100

    Update Carousel.js

--- Carousel.js
+++ Carousel.js
@@ -1,5 +1,4 @@
 var html = `
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 
