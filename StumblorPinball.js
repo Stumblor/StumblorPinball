91b16fa95dccef3cfccfe8478754b7cd59b876c1
Stumblor <davey@codercabana.co.uk>
2021-08-19 21:53:08 +0100

    Update StumblorPinball.js

--- StumblorPinball.js
+++ StumblorPinball.js
@@ -158,7 +158,7 @@ $( document ).ready(function() {
   var product = () => {
     if ($('.w-product-description').length == 0) return setTimeout(product, 500);
     var html = $('.w-product-description > span').html();
-    html = html.replace(/\*\*\*\*(.*)?\((.*?)\)\*\*\*\*/gi, "<div class=\"blockquote-wrapper\"><blockquote><h1>$1</h1><h4>$2</h4></blockquote></div>");
+    html = html.replace(/\*\*\*\*(.*)?\((.*?)\)\*\*\*\*/gi, "<blockquote><h1>$1</h1><h4>$2</h4></blockquote>");
     $('.w-product-description > span').html(html);
   }
   
