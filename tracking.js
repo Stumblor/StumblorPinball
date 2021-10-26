02f433c2ef213e6614c1bc7ab6d209500e52a765
Stumblor <davey@codercabana.co.uk>
2021-10-26 18:04:48 +0200

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,5 @@
+<script src="https://cdn.jsdelivr.net/gh/jquery/jquery@3.4.1/dist/jquery.min.js" crossorigin="anonymous"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" crossorigin="anonymous"> </script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FM7JMR8V4S"></script>
+<script async src="https://cdn.jsdelivr.net/gh/Stumblor/StumblorPinball@4bfa72f/StumblorPinball.js" crossorigin="anonymous"></script>
+
