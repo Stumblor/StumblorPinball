4bfa72fd426c0268f041f0a585aa498c316edbe7
Stumblor <davey@codercabana.co.uk>
2021-10-26 13:44:40 +0200

    Update StumblorPinball.js

--- StumblorPinball.js
+++ StumblorPinball.js
@@ -10,28 +10,31 @@ $( document ).ready(function() {
 
   //---------------------------------------
   // DRIFT
-  !function() {
-    var t = window.driftt = window.drift = window.driftt || [];
-    if (!t.init) {
-      if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-      t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-      t.factory = function(e) {
-        return function() {
-          var n = Array.prototype.slice.call(arguments);
-          return n.unshift(e), t.push(n), t;
+  // only load on non-kanding pages
+  var drift = () => {
+    !function() {
+      var t = window.driftt = window.drift = window.driftt || [];
+      if (!t.init) {
+        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+        t.factory = function(e) {
+          return function() {
+            var n = Array.prototype.slice.call(arguments);
+            return n.unshift(e), t.push(n), t;
+          };
+        }, t.methods.forEach(function(e) {
+          t[e] = t.factory(e);
+        }), t.load = function(t) {
+          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+          var i = document.getElementsByTagName("script")[0];
+          i.parentNode.insertBefore(o, i);
         };
-      }, t.methods.forEach(function(e) {
-        t[e] = t.factory(e);
-      }), t.load = function(t) {
-        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(o, i);
-      };
-    }
-  }();
-  drift.SNIPPET_VERSION = '0.3.1';
-  drift.load('4333vua8587v');  
+      }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('4333vua8587v');  
+  };
 
   // CUSTOM CSS
   var styleElement = document.createElement('style');
@@ -224,8 +227,12 @@ $( document ).ready(function() {
     var url = window.location.href;
     if (url != urlwas) {
       if (url == "https://www.stumblorpinball.com/") {
+        // landing page
         stories();
         quotes("#uzNwiC p");
+      } else {
+        // non landing pages
+        drift();
       }
       if (url.indexOf("https://www.stumblorpinball.com/product") >= 0) {
         quotes('.w-product-description > span', () => {
